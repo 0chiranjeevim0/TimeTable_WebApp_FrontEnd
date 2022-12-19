952c00f350f8eba2fba38a3490a19774374271ac
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2022-12-19 21:22:08 +0530

    status : firebase cd

---
+++ firebase/firebase.js
@@ -0,0 +1,10 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyByQavRImv-Jgs67GX5DZGPCYr8OQhqNJs",
+    authDomain: "timetable-application-testenv.firebaseapp.com",
+    projectId: "timetable-application-testenv",
+    storageBucket: "timetable-application-testenv.appspot.com",
+    messagingSenderId: "305940384860",
+    appId: "1:305940384860:web:abc606c471eb1436a5ea99"
+})
